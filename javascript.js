ecd8b0ed44d8f75cfa5ac2dad1980c122e1f9233
tangerine003 <123626883+tangerine003@users.noreply.github.com>
2023-04-28 21:40:52 +0530

    Add operate function
    The operate function directly takes the operator as an argument(along
    with 2 operands) without the user having to explicitly call one of
    the appropriate operator functions defined earlier in the code.

--- javascript.js
+++ javascript.js
@@ -11,3 +11,31 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      console.log(result);
+      break;
+
+    case "-":
+      result = subtract(num1, num2);
+      console.log(result);
+      break;
+
+    case "*":
+      result = multiply(num1, num2);
+      console.log(result);
+      break;
+
+    case "/":
+      result = divide(num1, num2);
+      console.log(result);
+      break;
+
+    default:
+      console.log("error");
+  }
+}
