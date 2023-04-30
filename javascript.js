2e8318e9a448ddca4f8e15963d86e3a556b48926
tangerine003 <123626883+tangerine003@users.noreply.github.com>
2023-04-30 21:23:07 +0530

    Call operate function when "=" button is pressed

--- javascript.js
+++ javascript.js
@@ -42,6 +42,7 @@ function operate(num1, operator, num2) {
 const num1 = document.createElement("h3");
 const operator = document.createElement("h3");
 const num2 = document.createElement("h3");
+const resultant = document.createElement("h3");
 const displayScreen = document.querySelector(".display");
 
 const calcButtons = document.querySelectorAll(".calc-button");
@@ -55,6 +56,12 @@ calcButtons.forEach((button) => {
         num1.textContent = e.target.getAttribute("data-value");
         displayScreen.appendChild(num1);
       }
+    } else if (e.target.getAttribute("data-value") == "=") {
+      resultant.textContent = operate(
+        num1.textContent,
+        operator.textContent,
+        num2.textContent
+      );
     } else {
       operator.textContent = e.target.getAttribute("data-value");
       displayScreen.appendChild(operator);
