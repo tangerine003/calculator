9c0ab08a67ade5b8c226042218ba4bb30f8363c9
tangerine003 <123626883+tangerine003@users.noreply.github.com>
2023-05-03 22:36:23 +0530

    Make the backspace button work correctly

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 function add(num1, num2) {
   return Math.round((num1 + num2 + Number.EPSILON) * 100) / 100;
 }
-
 function subtract(num1, num2) {
   return Math.round((num1 - num2 + Number.EPSILON) * 100) / 100;
 }
@@ -11,7 +10,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return Math.round((num1 / num2 + Number.EPSILON) * 100) / 100;
 }
-
 function operate(num1, operator, num2) {
   num1 = Number(num1);
   num2 = Number(num2);
@@ -25,22 +23,18 @@ function operate(num1, operator, num2) {
         result = add(num1, num2);
         return result;
         break;
-
       case "-":
         result = subtract(num1, num2);
         return result;
         break;
-
       case "*":
         result = multiply(num1, num2);
         return result;
         break;
-
       case "/":
         result = divide(num1, num2);
         return result;
         break;
-
       default:
         console.log("error");
     }
@@ -51,21 +45,16 @@ const operator = document.createElement("h3");
 const num2 = document.createElement("h3");
 const resultant = document.createElement("h3");
 const displayScreen = document.querySelector(".display");
-
 const errorMessage = document.createElement("h3");
 errorMessage.textContent = "ERROR";
-
 let noOfPointsInOperand1 = 0;
 let noOfPointsInOperand2 = 0;
-
 num1.textContent = 0;
 displayScreen.append(num1);
-
 const confusedEmoji = document.createElement("img");
 confusedEmoji.src = "./imgs/path1.png";
 confusedEmoji.width = 50;
 confusedEmoji.setAttribute("class", "astonished-face");
-
 const calcButtons = document.querySelectorAll(".calc-button");
 calcButtons.forEach((button) => {
   button.addEventListener("mousedown", function (e) {
@@ -201,7 +190,6 @@ calcButtons.forEach((button) => {
 });
 
 const topButtons = document.querySelectorAll(".top-button");
-
 topButtons.forEach((button) => {
   button.addEventListener("click", function (e) {
     let sound = new Audio("./audio-effects/select-sound-121244.mp3");
@@ -221,10 +209,22 @@ topButtons.forEach((button) => {
           noOfPointsInOperand1 = 0;
         }
         num1.textContent = textContentArray.join("");
+        if (num1.textContent == "") {
+          displayScreen.removeChild(num1);
+          num1.textContent = "0";
+          displayScreen.appendChild(num1);
+        }
       } else if (displayScreen.lastElementChild == operator) {
         const textContentArray = Array.from(operator.textContent);
         textContentArray.pop();
         operator.textContent = textContentArray.join("");
+        if (operator.textContent == "") {
+          displayScreen.removeChild(operator);
+          if (!displayScreen.textContent) {
+            num1.textContent = "0";
+            displayScreen.appendChild(num1);
+          }
+        }
       } else if (displayScreen.lastElementChild == num2) {
         const textContentArray = Array.from(num2.textContent);
         const PoppedElement = textContentArray.pop();
@@ -232,10 +232,18 @@ topButtons.forEach((button) => {
           noOfPointsInOperand2 = 0;
         }
         num2.textContent = textContentArray.join("");
+        if (num2.textContent == "") {
+          displayScreen.removeChild(num2);
+        }
       } else {
         const textContentArray = Array.from(resultant.textContent);
         textContentArray.pop();
         resultant.textContent = textContentArray.join("");
+        if (resultant.textContent == "") {
+          displayScreen.removeChild(resultant);
+          num1.textContent = "0";
+          displayScreen.appendChild(num1);
+        }
       }
     }
   });
